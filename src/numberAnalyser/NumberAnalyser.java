0a51259a61301b66c1aad72a4708f5698d69d5ec
Bernd Heidemann <bernd.heidemann@googlemail.com>
2022-10-11 09:12:25 +0200

    Palindrom & NumberAnalyser

--- src/numberAnalyser/NumberAnalyser.java
+++ src/numberAnalyser/NumberAnalyser.java
@@ -3,26 +3,76 @@ package numberAnalyser;
 public class NumberAnalyser {
 
     public int getGGT(int number1, int number2) {
-        return 0;
+        while (number2 != 0) {
+            if (number1 > number2) {
+                number1 = number1 - number2;
+            } else {
+                number2 = number2 - number1;
+            }
+        }
+        return number1;
     }
 
     public boolean isPrimeNumber(int number) {
-        return false;
+        if (number == 1) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public String getPrimeNumbers(int start, int end) {
-        return "";
+        String result = "";
+        for (; start <= end; start++) {
+            if (isPrimeNumber(start)) {
+                result += start + " ";
+            }
+        }
+        result = result.substring(0, result.length() - 1);
+        return result;
     }
 
     public int getChecksum(int number) {
-        return 0;
+        int sum = 0, digit;
+        do {
+            digit = number % 10;
+            number = number / 10;
+            sum += digit;
+        } while (number > 0);
+        return sum;
     }
 
     public boolean isHappyNumber(int number) {
-        return false;
+        int sum, digit;
+        do {
+            sum = 0;
+            do {
+                digit = number % 10;
+                number = number / 10;
+                sum = sum + digit * digit;
+            } while (number > 0);
+            number = sum;
+        } while (sum != 1 && sum != 4);
+
+        if (sum == 1)
+            return true;
+        else
+            return false;
     }
 
     public boolean isPerfectNumber(int number) {
-        return false;
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0)
+                sum += i;
+        }
+        if (number == sum)
+            return true;
+        else
+            return false;
     }
 }
--- src/palindrom/Palindrom.java
+++ src/palindrom/Palindrom.java
@@ -1,7 +1,51 @@
 package palindrom;
 
+import java.util.Locale;
+
 public class Palindrom {
-    public boolean isPalindrom(String text){
-        return false;
+    public boolean isPalindrom(String text) {
+        // liefert das Zeichen an der Position 1 (startet bei 0)
+        char c = text.charAt(1);
+        // liefert die Anzahl an Zeichen
+        int len = text.length();
+
+        // baue einen Text, nur mit Kleinbuchstaben und ohne Leerzeichen
+        String newText = text.replaceAll(" ", "");
+
+        // Das hier ist eine (umständliche) Alternative zum replaceAll
+        String newText2 = "";
+        // deklariere die Variable i und initialisiere sie mit 0   --> int i=0
+        // solange i kleiner als Anzahl Zeichen im Text            --> i<text.length()
+        // erhöhe i um 1                                           --> i++
+        for (int i = 0; i < text.length(); i++) {
+            // wenn an der Stelle i im Text KEIN Leerzeichen ist
+            if (text.charAt(i) != ' ') {
+                // füge dieses Zeichen dem String newText2 hinzu
+                newText2 += text.charAt(i);
+            }
+        }
+
+        // Großbuchstaben in Kleinbuchstaben ändern
+        newText = newText.toLowerCase();
+
+        // warum nur i<newText.length() / 2  -->
+        // wir prüfen ob die vorderen zeichen mit dm hintere übereinstimmen, daher
+        // genügt es, bis zur Hälfte zu vegleichen
+        for (int i = 0; i < newText.length() / 2; i++) {
+            // bei einem Text mit 10 Zeichen wäre beim ersten Durchlauf:
+            // 10-1-0 --> hinteres Zeichen hat index 9
+            // danach:
+            // 10-1-1 --> hinteres Zeichen hat index 8
+            // danach:
+            // 10-1-2 --> hinteres Zeichen hat index 7
+            int index2 = newText.length() - 1 - i;
+            // ist an irgendeiner Stelle keine Übereinstimmung:
+            if (newText.charAt(i) != newText.charAt(index2)) {
+                // kein Palindrom
+                return false;
+            }
+        }
+        // kommen wir hier hin, waren alle Zeichen gleich, d.h. Palindrim
+        return true;
     }
 }
